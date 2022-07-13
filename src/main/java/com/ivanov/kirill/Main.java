aaf3eb32f261f7d4e11081748ae1fc567361ebf2
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2022-07-13 19:06:35 +0300

    Sample

--- src/main/java/com/ivanov/kirill/Main.java
+++ src/main/java/com/ivanov/kirill/Main.java
@@ -1,7 +1,21 @@
 package com.ivanov.kirill;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatesToCronConverter converter = new DatesToCronConverter();
+        System.out.println(DatesToCronConverter.getImplementationInfo());
+        List<String> dates = List.of(
+                "2022-01-25T08:00:00",
+                "2022-01-25T08:30:00",
+                "2022-01-25T09:00:00",
+                "2022-01-25T09:30:00",
+                "2022-01-26T08:00:00",
+                "2022-01-26T08:30:00",
+                "2022-01-26T09:00:00",
+                "2022-01-26T09:30:00"
+        );
+        System.out.println(DatesToCronConverter.convert(dates));
     }
 }
