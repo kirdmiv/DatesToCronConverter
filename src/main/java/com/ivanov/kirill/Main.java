a95b4ec45dbad33910bc011051986b513becafa0
Kirill Ivanov <kirdmiv@users.noreply.github.com>
2022-07-14 18:50:18 +0300

    Fix sample

--- src/main/java/com/ivanov/kirill/Main.java
+++ src/main/java/com/ivanov/kirill/Main.java
@@ -1,20 +1,21 @@
 package com.ivanov.kirill;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(DatesToCronConverter.getImplementationInfo());
-//        List<String> dates = List.of(
-//                "2022-01-25T08:00:00",
-//                "2022-01-25T08:30:00",
-//                "2022-01-25T09:00:00",
-//                "2022-01-25T09:30:00",
-//                "2022-01-26T08:00:00",
-//                "2022-01-26T08:30:00",
-//                "2022-01-26T09:00:00",
-//                "2022-01-26T09:30:00"
-//        );
-//        System.out.println(DatesToCronConverter.convert(dates));
+        String[] data = {
+                "2022-01-25T08:00:00",
+                "2022-01-25T08:30:00",
+                "2022-01-25T09:00:00",
+                "2022-01-25T09:30:00",
+                "2022-01-26T08:00:00",
+                "2022-01-26T08:30:00",
+                "2022-01-26T09:00:00",
+                "2022-01-26T09:30:00"
+        };
+        System.out.println(DatesToCronConverter.convert(Arrays.asList(data)));
     }
 }
